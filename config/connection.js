255c6dd8f39507809636d89627e5064959ee47e0
Chris Paradeise <chris.paradeise@gmail.com>
2020-01-16 19:10:16 -0800

    Added database connection and initial schema

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Get MySQL up and running!
+const mysql = require('mysql');
+
+// Create Connection Object
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgeregrub_db'
+});
+
+// Connect
+connection.connect((err) => {
+    if (err) {
+        console.error("ERROR Connection to Database: " + err.stack);
+        return;
+    }
+    console.log("SUCCESS Connected to DB as Id " + connection.threadId);
+});
+
+// Export connectoin for our ORM to use
+module.exports = connection;
